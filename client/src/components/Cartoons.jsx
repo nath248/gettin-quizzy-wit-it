e13ba15bf1225d4a7f5951461f7964a1c157e33d
natherrera248 <nmherrera20@outlook.com>
2021-12-03 19:03:43 -0500

    updated component content

--- client/src/components/Cartoons.jsx
+++ client/src/components/Cartoons.jsx
@@ -1,7 +1,7 @@
 // import {useNavigate, useParams } from "react-router-dom";
 // import { useState, useEffect } from "react";
 
-function Cartoons() {
+function Cartoons(props) {
 
   // const navigate = useNavigate();
   // const params = useParams();
@@ -14,10 +14,21 @@ function Cartoons() {
   //   })
   //   setCategory(findCategory);
   // }, [])
-
+  // const content = props.questions.map((question) => question.fields.category === "cartoon";
+  // console.log(content);
   return (
     <div>
-      
+      {props.questions[0] ?
+            <>
+            <h3 key={props.questions.id}>{props.questions[0].fields.question}</h3>
+              <button>{props.questions[0].fields.answer1}</button>
+              <button>{props.questions[0].fields.answer2}</button>
+              <button>{props.questions[0].fields.answer3}</button>
+              <button>{props.questions[0].fields.answer4}</button>
+        </>
+        :
+      null
+      }
     </div>
   )
 }
