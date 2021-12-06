1a2bcb2e1a46a4968e2903914a1f904b596216e8
natherrera248 <nmherrera20@outlook.com>
2021-12-06 18:15:17 -0500

    added percent to results

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 
 function Results(props) {
-
+  const percent = (props.score / props.total) * 100;
 
   return (
     <div>
       <h1>Results</h1>
-      <p>You got {props.score}/{props.total} correct</p>
+      <p>{percent}%</p>
+      <p>You got {props.score}/{props.total} questions correct</p>
       <p>Want to add questions to this quiz? Click ADD below.</p>
       <Link to="/new">ADD</Link>
     </div>
