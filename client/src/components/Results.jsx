a1503ec210aaa3b290839ebb54d806982f89b1ac
natherrera248 <nmherrera20@outlook.com>
2021-12-08 10:45:17 -0500

    edit to questions styling

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -8,12 +8,14 @@ function Results(props) {
   }
 
   return (
-    <div>
+    <div className="results">
       <h1>Results</h1>
-      <p>{percent}%</p>
-      <p>You got {props.score}/{props.total} questions correct</p>
-      <p>Want to add questions to this quiz? Click ADD below.</p>
-      <Link to="/new" onClick={reset}>ADD</Link>
+      <div className="results-content">
+        <p>{percent}%</p>
+        <p>You got {props.score}/{props.total} questions correct</p>
+        <p>Want to add questions to this quiz? Click ADD below.</p>
+        <Link to="/new" onClick={reset}>ADD</Link>
+      </div>
     </div>
   )
 }
