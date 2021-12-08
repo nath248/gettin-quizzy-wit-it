54c8b11aad8d06c7133f2c75e8756b244ad2cc47
natherrera248 <nmherrera20@outlook.com>
2021-12-08 14:22:24 -0500

    updated footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,21 @@
 function Footer() {
   return (
     <div className="footer-div">
-      <li>
-        <a href="https://www.linkedin.com/in/nathaly-h-b5b02a64/" target="_blank" rel='noreferrer'>
-          <i className="fab fa-linkedin" aria-hidden="true"></i>
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/nath248" target="_blank" rel='noreferrer'>
-        <i className="fab fa-github" aria-hidden="true"></i>
-        </a>
-      </li>
+      <div className="icons">
+        <li>
+          <a href="https://www.linkedin.com/in/nathaly-h-b5b02a64/" target="_blank" rel='noreferrer'>
+            <i className="fab fa-linkedin" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/nath248" target="_blank" rel='noreferrer'>
+          <i className="fab fa-github" aria-hidden="true"></i>
+          </a>
+        </li>
+      </div>
+      <div className="copyright">
+        <small>&copy; Copyright 2021, Nathaly Herrera</small>
+      </div>
     </div>
   )
 }
